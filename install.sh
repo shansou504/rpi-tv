b89a964f48be072e6cdd34457abed0c125aa8b9d
shansou504 <123512155+shansou504@users.noreply.github.com>
2024-01-30 08:38:28 -0500

    changed mjpg-streamer with motion

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo raspi-config
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y xinit openbox obsession tint2 vim pulseaudio pavucontrol pasystray network-manager-gnome blueman lxpolkit lxterminal lxappearance gnome-themes-extra papirus-icon-theme feh rpd-plym-splash git suckless-tools onboard antimicro retroarch libretro-mgba libretro-gambatte kodi kodi-inputstream-adaptive kodi-peripheral-joystick cmake libjpeg-dev unattended-upgrades
+sudo apt install -y xinit openbox obsession tint2 vim pulseaudio pavucontrol pasystray network-manager-gnome blueman lxpolkit lxterminal lxappearance gnome-themes-extra papirus-icon-theme feh rpd-plym-splash git suckless-tools onboard antimicro retroarch libretro-mgba libretro-gambatte kodi kodi-inputstream-adaptive kodi-peripheral-joystick unattended-upgrades motion
 cd
 git clone https://github.com/shansou504/raspberry-pi-tv
 cd raspberry-pi-tv/
@@ -15,15 +15,11 @@ echo "Copying scripts into ~/.local/bin/"
 cp -r local/bin/* ~/.local/bin/
 chmod 755 ~/.local/bin/*
 cd
-git clone https://github.com/jacksonliam/mjpg-streamer.git
-cd mjpg-streamer-experimental
-echo "Making mjpg_streamer"
-make
-echo "Installing mjpg_streamer"
-sudo make install
-cd ~/raspberry-pi-tv/
-sudo cp mjpg_streamer.service /lib/systemd/system/
-sudo chown root:root /lib/systemd/system/mjpg_streamer.service
+echo "Fixing error with motion permissions and disabling service"
+sudo mkdir -p /var/log/motion/
+sudo touch /var/log/motion/motion.log
+sudo chown motion:motion -R /var/log/motion
+sudo systemctl disable motion
 mkdir ~/roms/
 echo "Adding startx to ~/.profile"
 cat autostartx >> ~/.profile
